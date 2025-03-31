c65bfb3dd5ecd70e94af8d337d70a7cd0b16070a
SimonHic <simonhicdev@gmail.com>
2025-03-31 23:01:43 +0100

    Completed Fan out implementation - added emailing logic

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SES_REGION = 'eu-west-1';
+export const SES_EMAIL_FROM = 'simonhicdev@gmail.com' ;
+export const SES_EMAIL_TO =  'simonhicdev@gmail.com';
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -28,6 +28,10 @@ export class EDAAppStack extends cdk.Stack {
     receiveMessageWaitTime: cdk.Duration.seconds(5),
   });
 
+  const mailerQ = new sqs.Queue(this, "mailer-queue", {
+    receiveMessageWaitTime: cdk.Duration.seconds(10),
+  });
+
   const newImageTopic = new sns.Topic(this, "NewImageTopic", {
     displayName: "New Image topic",
   }); 
@@ -35,6 +39,8 @@ export class EDAAppStack extends cdk.Stack {
   newImageTopic.addSubscription(
     new subs.SqsSubscription(imageProcessQueue)
   );
+
+  newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
   // Lambda functions
 
@@ -49,6 +55,26 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+  
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
+
+
   // S3 --> SQS
   imagesBucket.addEventNotification(
     s3.EventType.OBJECT_CREATED,
@@ -61,7 +87,13 @@ export class EDAAppStack extends cdk.Stack {
     maxBatchingWindow: cdk.Duration.seconds(5),
   });
 
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(5),
+  }); 
+
   processImageFn.addEventSource(newImageEventSource);
+  mailerFn.addEventSource(newImageMailEventSource);
 
   // Permissions
 
